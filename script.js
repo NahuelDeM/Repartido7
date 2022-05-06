e8e7ebb97c0de3473d5c1eabd0ace6d0f29a450d
NahuelDeM <Bios.Nahuel@gmail.com>
2022-05-06 17:58:31 -0300

    modulo2

---
+++ script.js
@@ -0,0 +1,46 @@
+// Ejercicio 1
+/* var famosos = [];
+        do {
+            x = prompt("Ingrese famosos: ");
+            famosos.push(x)
+        } while (x != ""); */
+
+// Ejercicio 2
+/* function invertircadena(ilv) {
+
+    var separarCadena = ilv.split("");
+    
+
+    separarCadena.reverse();
+    
+    var unirArreglo = separarCadena.join("");
+    
+    return unirArreglo;
+}
+invertircadena("Hola"); */
+
+// Ejercicio 3
+/* function separarCadena(ilv) {
+
+    var separarCadena = ilv.split("");
+
+    var ordenar = separarCadena.sort();
+
+    var juntar = ordenar.join("");
+    
+    return ordenar;
+}
+separarCadena("nahuel");  */
+
+// Ejercicio 4
+/* var lista = []
+for (let i = 0; i < 50; i++) {
+    lista.push(i + 1)           
+}
+function pares(ilv) {
+    var filtrado = lista.filter(n => n % 2 == 0);
+    return filtrado
+}
+pares(lista) */
+
+// Ejercicio 5
